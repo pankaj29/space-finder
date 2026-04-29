2105fdaa90113fb02cf6e4c70272d6fdf96e782b
Pankaj Verma <pankaj.webmaster@gmail.com>
2026-04-29 19:18:37 +0530

    lambda

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,12 +1,11 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { ITable } from 'aws-cdk-lib/aws-dynamodb';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Runtime} from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
 import { join } from 'path';
-import { Effect } from 'aws-cdk-lib/aws-iam';
 
 interface LambdaStackProps extends StackProps {
     spacesTable: ITable
@@ -14,32 +13,22 @@ interface LambdaStackProps extends StackProps {
 
 export class LambdaStack extends Stack {
 
-    public readonly helloLambdaIntegration: LambdaIntegration
+    public readonly spacesLambdaIntegration: LambdaIntegration
 
     constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props)
 
 
-        const helloLambda = new NodejsFunction(this, 'HelloLambda', {
+        const spacesLambda = new NodejsFunction(this, 'SpacesLambda', {
             runtime: Runtime.NODEJS_18_X,
             handler: 'handler',
-            entry: (join(__dirname, '..','..', 'services', 'hello.ts')),
+            entry: (join(__dirname, '..','..', 'services', 'spaces', 'handler.ts')),
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
             }
         });
 
-        helloLambda.addToRolePolicy(new PolicyStatement({
-            effect: Effect.ALLOW,
-            actions:['s3:ListAllMyBuckets',
-                    's3:ListBucket',
-                    's3:GetBucketLocation',
-                    's3:GetObject'
-            ],
-            resources: ['*']
-        }))
-
-        this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
+        this.spacesLambdaIntegration = new LambdaIntegration(spacesLambda)
 
     }
 }
